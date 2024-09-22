848b3ab4f7e6535cfc50beb79252452c0b4b7499
lhbchun <71207014+lhbchun@users.noreply.github.com>
2024-09-23 00:09:18 +0800

    Add columns and frame

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { JsxElement } from 'typescript';
 
 interface YouTubeEmbedProps {
   videoId: string;
@@ -8,6 +9,13 @@ interface YouTubeEmbedProps {
   height?: number;
   autoplay?: boolean;
 }
+
+interface RibbonButton {
+  text: string;
+  directory: string;
+}
+
+var songButton = {text: 'Songs', directory: '/songs'};
 
 var defaultEmbedProps: YouTubeEmbedProps = {
   videoId: 'Lg0Djk3DoQI',
@@ -17,11 +25,18 @@ var defaultEmbedProps: YouTubeEmbedProps = {
 };
 
 function App(){
+  let testVideo = {videoId: 'Lg0Djk3DoQI', width: 100, height: 100, autoplay: true};
+  // dark grey background on everything
   return (
-    <div>
-      <h1>My YouTube Video</h1>
-      <YouTubeEmbed videoId="Lg0Djk3DoQI" width={100} height={100} autoplay={true} />
+    <div className="two-column">
+      <div className="column">
+        {createMenu()}
+      </div>
+      <div className="column">
+        {VideoPlayer(YouTubeEmbed(testVideo))}
+      </div>
     </div>
+
   );
 };
 
@@ -46,5 +61,33 @@ function YouTubeEmbed(videoInfo: YouTubeEmbedProps = defaultEmbedProps) {
   );
 }
 
+function VideoPlayer(videoInput: ReactNode){
+  // a 960px x 540 px frane
+  // The top 960px x 440px should be the video player, left the rest empty for now {videoInput}
+  return(
+    <div style={{width: '640px', height: '480px', margin: 'auto', textAlign: 'center', border: '10px solid red',borderRadius: '10px',  padding: '10px'}}>
+      <div className="two-row">
+        <div style={{height: 'calc(100% - 100px)'}}>{videoInput}</div>
+        <div></div>
+        
+      </div>
+    </div>
+  );
+}
+
+function PageTitle(){
+  return(
+    <h1>Karaomake</h1>
+  );
+}
+
+function createMenu(){
+  // create a menu frame on the left side of the UI, it should be grey Color
+  return(
+    <div style={{backgroundColor: 'grey', width: '150px', height: '100%', position: 'fixed', top: 0, left: 0}}>
+      <div style={{backgroundColor: 'grey', color: 'white', width: '100px', height: '50px', textAlign: 'center', lineHeight: '50px', margin: 'auto'}}>Songs</div>
+    </div>
+  );
+}
 
 export default App;
